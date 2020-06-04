7746f4d765ae440a15a5c93465c951250076594b
Naveenkadari07 <naveenkadari9898@gmail.com>
2020-06-04 07:20:02 +0530

    Initial commit

---
+++ list_operations.py
@@ -0,0 +1,29 @@
+num = [12,16,22,28,36,54,9]
+print(num)
+print(num[0])
+print(num[1:])
+print(num[-2:])
+names = ['Kamalhassan','rajinikanth','abhi']
+print(names)
+values = [0.1,'abhi',9]
+print(values)
+print(num,names)
+(num.remove(28))
+print(num)
+(num.append(40))
+print(num)
+(num.insert(4,40))
+print(num)
+print(min(num))
+print(max(num))
+print(sum(num))
+(num.sort())
+print(num)
+print(num[0])
+(num[0])=43
+print(num[0])
+print(len(num))
+print([1,2,3,4,6,100] + [12,7,8,9,200])
+print(['hello']*3)
+N = [1,2,3,5,5,6,3,6,6,6]
+print(N.count(6))
